29de4de0b04da2e30620f6b3c345202706473439
PatiKSMRZ <patr.kusmierz@gmail.com>
2026-01-19 12:36:10 +0100

    firebase config, navigation configuration

--- App.tsx
+++ App.tsx
@@ -1,16 +1,12 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import HomeScreen from './src/screens/HomeScreen';
-import { colors } from './src/theme/colors';
+import RootNavigator from './src/navigation/RootNavigator';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <SafeAreaView style={{ flex: 1, backgroundColor: colors.item.background }}>
-        <StatusBar barStyle="dark-content" backgroundColor={colors.item.background} />
-        <HomeScreen />
-      </SafeAreaView>
+      <RootNavigator />
     </NavigationContainer>
   );
 }
